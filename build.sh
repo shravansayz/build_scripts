1df4d3c15dc9594ec686628ac71a58554415ffcb
Shravankumar <shravankumarsayz@gmail.com>
2024-05-08 17:42:43 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,15 +4,14 @@
 rm -rf .repo/local_manifests
 
 #sync
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --depth=1
-git clone https://github.com/shravansayz/local_manifests.git -b rising .repo/local_manifests
+repo init -u https://github.com/StatiXOS/android_manifest.git -b udc-qpr2 --git-lfs --depth=1
+git clone https://github.com/shravansayz/local_manifests.git -b test .repo/local_manifests
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 #ksu
-cd kernel/kernel/sdm710 && curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash - && cd ../../..
+cd kernel/realme/sdm710 && curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash - && cd ../../..
 
 # build
 /opt/crave/resync.sh
 . build/envsetup.sh
-riseup RMX1901 user
-rise b
+brunch RMX1901-ap1a-user
