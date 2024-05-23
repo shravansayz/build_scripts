25e5963657da18b72851561e437aec17c2eda983
Shravankumar <shravankumarsayz@gmail.com>
2024-05-23 14:16:39 +0530

    Create lineage.sh

---
+++ lineage.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#removals
+rm -rf .repo/local_manifests
+
+#sync
+repo init -u https://github.com/Los-Ext/manifest.git -b lineage-21.0 --git-lfs --depth=1
+git clone https://github.com/shravansayz/local_manifests.git -b los21 .repo/local_manifests
+if [ -f /opt/crave/resync.sh ]; then
+  /opt/crave/resync.sh
+else
+  repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+fi
+
+#customs
+rm -rf frameworks/base
+https://github.com/shravansayz/android_frameworks_base_los_ext.git -b lineage-21.0 frameworks/base --depth=1
+
+#ksu
+cd kernel/realme/sdm710 && curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash - && cd ../../..
+
+# build
+. build/envsetup.sh
+lunch lineage_RMX1901-ap1a-userdebug
+m installclean
+m bacon
+
+#crave build command
+#crave run --no-patch "rm -rf lineage.sh && wget https://raw.githubusercontent.com/shravansayz/build_scripts/pos/lineage.sh && bash lineage.sh"
