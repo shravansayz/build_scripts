f4458dbf4cb4759f79c62b7fa42865ab5e7eb884
Shravankumar <shravankumarsayz@gmail.com>
2024-06-02 10:18:34 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,6 +16,12 @@ fi
 rm -rf frameworks/base
 git clone https://github.com/shravansayz/frameworks_base_pos.git -b fourteen frameworks/base --depth=1
 
+rm -rf vendor/extra
+git clone https://github.com/shravansayz/vendor_extra -b main vendor/extra
+
+#rm -rf vendor/aosp/signing/keys
+#git clone https://github.com/shravansayz/vendor_sign-priv -b pos vendor/aosp/signing/keys
+
 #ksu
 cd kernel/realme/sdm710 && curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash - && cd ../../..
 
