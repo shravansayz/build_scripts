44a1a9e1aa5d0d1ea01490af7e9bd332ae4d5aed
Shravankumar <shravankumarsayz@gmail.com>
2024-06-07 04:37:37 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,16 +17,10 @@ rm -rf frameworks/base
 git clone https://github.com/shravansayz/android_frameworks_base_rise.git -b fourteen frameworks/base --depth=1
 
 # Private Keys
-git clone https://github.com/shravansayz/vendor_lineage-priv vendor/lineage-priv
+#git clone https://github.com/shravansayz/vendor_lineage-priv vendor/lineage-priv
 
 # Set up build environment
 source build/envsetup.sh
 genkeys shravankumarsayz@gmail.com
-# Lunch configuration
 riseup RMX1901 user
-
-# Cleanup directories
-make installclean
-
-# Build
 rise sb
