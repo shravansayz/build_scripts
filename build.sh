734b2260bf46e46136c9b91ef452c036fed695b2
Shravankumar <shravankumarsayz@gmail.com>
2024-06-06 16:44:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#removals
+# Removals
 rm -rf .repo/local_manifests
 
 # Initialize repo with specified manifest
@@ -9,25 +9,24 @@ repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --de
 # Clone local_manifests repository
 git clone https://github.com/shravansayz/local_manifests --depth 1 -b rise .repo/local_manifests
 
-# Resync the repositories
-/opt/crave/resync.sh
+# Sync the repositories
+curl https://raw.githubusercontent.com/sounddrill31/docker-images/04449990912b9d7ee594193883740037f0ac80a7/aosp/common/resync.sh | bash
 
 #customs
 rm -rf frameworks/base
 git clone https://github.com/shravansayz/android_frameworks_base_rise.git -b fourteen frameworks/base --depth=1
 
 # Private Keys
-rm -rf vendor/lineage-priv && git clone https://github.com/shravansayz/vendor_lineage-priv vendor/lineage-priv
+git clone https://github.com/shravansayz/vendor_lineage-priv vendor/lineage-priv
 
 # Set up build environment
 source build/envsetup.sh
-genkeys shravankumarsayz@gmail.com
 
 # Lunch configuration
 riseup RMX1901 user
+
+# Cleanup directories
 make installclean
 
 # Build
-croot
-repo forall -c 'git lfs install && git lfs pull && git lfs checkout'
-rise sb
+rise b
