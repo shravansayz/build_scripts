b1dd2dc5c7d8579752a9adcdffb7818cea602c15
Shravankumar <shravankumarsayz@gmail.com>
2024-06-08 15:23:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,13 +16,8 @@ curl https://raw.githubusercontent.com/sounddrill31/docker-images/04449990912b9d
 rm -rf frameworks/base
 git clone https://github.com/shravansayz/android_frameworks_base_rise.git -b fourteen frameworks/base --depth=1
 
-# Private Keys
-rm -rf vendor/lineage-priv && git clone https://github.com/shravansayz/vendor_lineage-priv -b main vendor/lineage-priv
-
 # Set up build environment
 source build/envsetup.sh
 riseup RMX1901 user
 make install clean
-# Git-lfs
-repo forall -c 'git lfs install && git lfs pull && git lfs checkout'
 rise b
