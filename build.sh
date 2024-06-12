21fa0f5c19b002921de8b80ba4f1e89f6d3b3471
Shravankumar <shravankumarsayz@gmail.com>
2024-06-12 07:11:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,20 +4,22 @@
 rm -rf .repo/local_manifests
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --depth=1
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs --depth=1
 
 # Clone local_manifests repository
-git clone https://github.com/shravansayz/local_manifests --depth 1 -b rise .repo/local_manifests
+git clone https://github.com/shravansayz/local_manifests --depth 1 -b pos14 .repo/local_manifests
 
 # Sync the repositories
 curl https://raw.githubusercontent.com/sounddrill31/docker-images/04449990912b9d7ee594193883740037f0ac80a7/aosp/common/resync.sh | bash
 
 #customs
 rm -rf frameworks/base
-git clone https://github.com/shravansayz/android_frameworks_base_rise.git -b fourteen frameworks/base --depth=1
+git clone https://github.com/shravansayz/frameworks_base_pos.git -b fourteen frameworks/base --depth=1
 
-# Set up build environment
-source build/envsetup.sh
-riseup RMX1901 user
-make install clean
-rise b
+#build
+. build/envsetup.sh
+lunch aosp_RMX1901-ap1a-userdebug
+mka target-files-package otatools
+
+#sign build
+rm -rf sign.sh && wget https://raw.githubusercontent.com/shravansayz/build_scripts/pos/sign.sh && bash sign.sh
