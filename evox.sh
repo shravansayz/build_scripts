9f6000cbc5717d2cb615cc78d766d5304b6d45fa
Shravankumar <shravankumarsayz@gmail.com>
2024-06-13 18:04:32 +0530

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -17,7 +17,7 @@ rm -rf frameworks/base
 git clone https://github.com/shravansayz/frameworks_base_evox.git -b udc frameworks/base --depth=1
 
 # Private Keys
-git clone https://github.com/shravansayz/private_keys_evox.git -b main vendor/evolution-priv
+wget https://github.com/bagaskara815/local_manifests/raw/keys/keys.zip && unzip -o keys.zip -d vendor/evolution-priv && rm keys.zip
 
 # Set up build environment
 source build/envsetup.sh
