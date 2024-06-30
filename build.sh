7574f3f7a9d41611b7a58e2a0a5dae2dc7530af0
Shravankumar <shravankumarsayz@gmail.com>
2024-06-30 11:20:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,22 +4,28 @@
 rm -rf .repo/local_manifests
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs --depth=1
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs
 
 # Clone local_manifests repository
 git clone https://github.com/shravansayz/local_manifests --depth 1 -b pos14 .repo/local_manifests
 
 # Sync the repositories
-curl https://raw.githubusercontent.com/sounddrill31/docker-images/04449990912b9d7ee594193883740037f0ac80a7/aosp/common/resync.sh | bash
+/opt/crave/resync.sh
 
 #customs
 rm -rf frameworks/base
 git clone https://github.com/shravansayz/frameworks_base_pos.git -b fourteen frameworks/base --depth=1
 
-#build
-. build/envsetup.sh
-lunch aosp_RMX1901-ap1a-userdebug
-mka target-files-package otatools
+export BUILD_USERNAME=shravan
+export BUILD_HOSTNAME=crave
+subject='/C=IN/ST=Haryana/L=Panipat/O=Android/OU=Android/CN=Android/emailAddress=shravansayz@gmail.com'
+for x in releasekey platform shared media networkstack nfc verity otakey testkey sdk_sandbox bluetooth; do \
+  echo '' |./development/tools/make_key vendor/aosp/signing/keys/$x "$subject"; \
+done
 
-#sign build
-rm -rf sign.sh && wget https://raw.githubusercontent.com/shravansayz/build_scripts/pos/sign.sh && bash sign.sh
+#build
+source build/envsetup.sh
+lunch aosp_RMX1901-ap2a-userdebug
+make installclean
+mka bacon
+
