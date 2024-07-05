b0156c71420a82a901d793e3afe6c05fa0958175
Shravankumar <shravankumarsayz@gmail.com>
2024-07-05 06:17:43 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 rm -rf .repo/local_manifests
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs
+repo init -u https://gitlab.com/ThankYouHonami/manifest.git -b fourteen --git-lfs
 
 # Clone local_manifests repository
 git clone https://github.com/shravansayz/local_manifests --depth 1 -b pos14 .repo/local_manifests
@@ -13,8 +13,8 @@ git clone https://github.com/shravansayz/local_manifests --depth 1 -b pos14 .rep
 /opt/crave/resync.sh
 
 #customs
-rm -rf frameworks/base
-git clone https://github.com/shravansayz/frameworks_base_pos.git -b fourteen frameworks/base --depth=1
+#rm -rf frameworks/base
+#git clone https://github.com/shravansayz/frameworks_base_pos.git -b fourteen frameworks/base --depth=1
 
 # Private Keys
 wget https://github.com/shravansayz/local_manifests/raw/keys/keys.zip && unzip -o keys.zip -d vendor/aosp/signing/keys && rm keys.zip
