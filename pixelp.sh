c8b05cf651dd332370b1255d52b09033aa0c9355
Shravankumar <shravankumarsayz@gmail.com>
2024-07-20 10:18:08 +0530

    Create pixelp.sh

---
+++ pixelp.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Removals
+rm -rf .repo/local_manifests
+
+# Initialize repo with specified manifest
+repo init -u https://github.com/The-Pixel-Project/manifest -b fourteen-qpr3 -g default,-mips,-darwin,-notdefault --git-lfs
+
+# Clone local_manifests repository
+git clone https://github.com/shravansayz/local_manifests --depth 1 -b pixelp14 .repo/local_manifests
+
+# Sync the repositories
+/opt/crave/resync.sh
+
+#customs
+rm -rf frameworks/base
+git clone https://github.com/shravansayz/frameworks_base_pixelpro.git -b fourteen-qpr3 frameworks/base --depth=1
+
+# Private Keys
+git clone https://github.com/shravansayz/test_keys.git -b main vendor/aosp/signing/keys
+
+export BUILD_USERNAME=shravan
+export BUILD_HOSTNAME=crave
+
+#build
+source build/envsetup.sh
+lunch aosp_RMX1901-user
+make installclean
+make bacon
