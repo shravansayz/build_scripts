393d9e4481997545abd46486515ae9de2359ac9e
Shravankumar <shravankumarsayz@gmail.com>
2024-07-29 11:05:18 +0530

    Update pixelp.sh

--- pixelp.sh
+++ pixelp.sh
@@ -4,7 +4,7 @@
 rm -rf .repo/local_manifests
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/The-Pixel-Project/manifest -b fourteen-qpr3 -g default,-mips,-darwin,-notdefault --git-lfs
+repo init -u https://github.com/Project-PixelStar/manifest -b 14-qpr3
 
 # Clone local_manifests repository
 git clone https://github.com/shravansayz/local_manifests --depth 1 -b pixelp14 .repo/local_manifests
@@ -14,17 +14,17 @@ git clone https://github.com/shravansayz/local_manifests --depth 1 -b pixelp14 .
 
 #customs
 rm -rf frameworks/base
-git clone https://github.com/shravansayz/frameworks_base_pixelpro.git -b fourteen-qpr3 frameworks/base --depth=1
+git clone https://github.com/shravansayz/frameworks_base_pixelstar.git -b 14-qpr3 frameworks/base --depth=1
 
 # Private Keys
-rm -rf vendor/aosp/signing
-git clone https://github.com/shravansayz/test_keys.git -b main vendor/aosp/signing
+rm -rf vendor/pixelstar/signing
+git clone https://github.com/shravansayz/private_keys.git -b pixelpro vendor/pixelstar/signing
 
 export BUILD_USERNAME=shravan
 export BUILD_HOSTNAME=crave
 
 #build
 source build/envsetup.sh
-lunch aosp_RMX1901-user
+lunch pixelstar_RMX1901-user
 make installclean
-make bacon
+mka bacon
