ee1530f209170d25b61132a248b295f076985d75
Shravankumar <shravankumarsayz@gmail.com>
2024-08-29 19:57:40 +0530

    Update lineage.sh

--- lineage.sh
+++ lineage.sh
@@ -14,7 +14,7 @@ git clone https://github.com/shravansayz/local_manifests --depth 1 -b ext .repo/
 
 #customs
 rm -rf frameworks/base
-git clone https://github.com/shravansayz/android_frameworks_base_los.git -b lineage-21.0 frameworks/base --depth=1
+git clone https://github.com/sksayz5/android_frameworks_base_Los.git -b main frameworks/base --depth=1
 
 #Private Keys
 rm -rf vendor/lineage-priv
