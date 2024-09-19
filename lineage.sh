53cd03ae67300511e67ce28e17da67c612f98d67
Shravankumar <shravankumarsayz@gmail.com>
2024-09-19 08:36:32 +0530

    Update lineage.sh

--- lineage.sh
+++ lineage.sh
@@ -4,7 +4,7 @@
 rm -rf .repo/local_manifests
 
 # Initialize repo with specified manifest
-repo init --depth=1 --no-repo-verify -u https://github.com/Los-Ext/manifest.git -b 14R --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Los-Ext/manifest.git -b lineage-21.0 --git-lfs -g default,-mips,-darwin,-notdefault
 
 # Clone local_manifests repository
 git clone https://github.com/shravansayz/local_manifests --depth 1 -b ext .repo/local_manifests
