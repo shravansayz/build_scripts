45882897a959c4ba360b891c162f47d1a9ef22a8
Shravankumar <shravankumarsayz@gmail.com>
2024-09-23 15:00:56 +0530

    Update yaap.sh

--- yaap.sh
+++ yaap.sh
@@ -9,11 +9,8 @@ repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
 # Clone local_manifests repository
 git clone https://github.com/shravansayz/local_manifests.git --depth 1 -b yaap-test .repo/local_manifests
 
-if [ -f /opt/crave/resync.sh ]; then
-  /opt/crave/resync.sh
-else
-  repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
-fi
+# Sync the repositories
+/opt/crave/resync.sh
 
 export BUILD_USERNAME=shravan
 export BUILD_HOSTNAME=crave
