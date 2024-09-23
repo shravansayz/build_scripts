989b8aaba4e8ca4cc960a2d4566377b3560f0775
Shravankumar <shravankumarsayz@gmail.com>
2024-09-23 15:17:25 +0530

    Update yaap.sh

--- yaap.sh
+++ yaap.sh
@@ -10,13 +10,16 @@ repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
 git clone https://github.com/shravansayz/local_manifests.git --depth 1 -b yaap-test .repo/local_manifests
 
 # Sync the repositories
-/opt/crave/resync.sh
+/opt/crave/resync.sh && /opt/crave/resync.sh
 
 export BUILD_USERNAME=shravan
 export BUILD_HOSTNAME=crave
 
+# Git-lfs
+repo forall -c 'git lfs install && git lfs pull'
+
 # Set up build environment
 source build/envsetup.sh
 lunch yaap_RMX1901-user
-m installclean
+make installclean
 m yaap
