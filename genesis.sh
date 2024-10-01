9b89cecd5192672ef4179d60ff931110b7da7a57
Shravankumar <shravankumarsayz@gmail.com>
2024-10-01 11:12:54 +0530

    genesis.sh

--- genesis.sh
+++ genesis.sh
@@ -4,13 +4,17 @@
 rm -rf .repo/local_manifests
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/ProjectBlaze/manifest -b 15 --git-lfs
+repo init -u https://github.com/GenesisOS/manifest.git -b utopia-3.0 --git-lfs
 
 # Clone local_manifests repository
 git clone https://github.com/shravansayz/local_manifests.git --depth 1 -b blaze  .repo/local_manifests
 
 # Sync the repositories
 /opt/crave/resync.sh && /opt/crave/resync.sh
+
+#Private Keys
+rm -rf vendor/genesis/signing
+git clone https://github.com/shravansayz/private_keys.git -b rise vendor/genesis/signing
 
 export BUILD_USERNAME=shravan
 export BUILD_HOSTNAME=crave
@@ -20,6 +24,6 @@ repo forall -c 'git lfs install && git lfs pull'
 
 # Set up build environment
 source build/envsetup.sh
-lunch blaze_RMX1901-ap3a-userdebug
+lunch genesis_RMX1901-user
 make installclean
-make bacon
+mka genesis
