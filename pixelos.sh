27eb1316506722c6d79757e3a0dee476cc469322
Shravankumar <shravankumarsayz@gmail.com>
2024-12-03 10:27:31 +0530

    Update pixelos.sh

--- pixelos.sh
+++ pixelos.sh
@@ -4,13 +4,17 @@
 rm -rf .repo/local_manifests
 
 # Initialize repo with specified manifest
-repo init --depth=1 -u https://gitlab.com/ThankYouHonami/manifest.git -b fifteen --git-lfs
+repo init --depth=1 -u https://github.com/PixelOS-Fifteen/manifest.git -b fifteen --git-lfs
 
 # Clone local_manifests repository
 git clone https://github.com/shravansayz/local_manifests.git --depth 1 -b pixel .repo/local_manifests
 
 # Sync the repositories
 /opt/crave/resync.sh
+
+#customs
+rm -rf frameworks/base
+git clone -b fifteen https://github.com/shravansayz/frameworks_base_pixelos.git frameworks/base --depth=1
 
 #Private Keys
 rm -rf vendor/aosp/signing
@@ -19,11 +23,8 @@ git clone https://github.com/shravansayz/private_keys.git -b rise vendor/aosp/si
 export BUILD_USERNAME=shravan
 export BUILD_HOSTNAME=crave
 
-# Git-lfs
-repo forall -c 'git lfs install && git lfs pull'
-
 # Set up build environment
 source build/envsetup.sh
-lunch aosp_RMX1901-ap3a-userdebug
+lunch aosp_RMX1901-ap3a-user
 make installclean
 mka bacon
