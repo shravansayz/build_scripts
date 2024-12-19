b96b1e35947ba3566779e291d8e8631ca5dcbd42
Shravankumar <shravankumarsayz@gmail.com>
2024-12-19 17:02:34 +0530

    Update derp.sh

--- derp.sh
+++ derp.sh
@@ -1,30 +1,16 @@
 #!/bin/bash
 
-# Removals
-rm -rf .repo/local_manifests
-
-# Initialize repo with specified manifest
-repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14 --depth=1 --git-lfs
-
-# Clone local_manifests repository
-git clone https://github.com/shravansayz/local_manifests --depth 1 -b derp14 .repo/local_manifests
-
-# Sync the repositories
-/opt/crave/resync.sh
-
-#customs
-rm -rf frameworks/base
-git clone https://github.com/shravansayz/frameworks_base_derp.git -b 14 frameworks/base --depth=1
-
-#Private Keys
-rm -rf vendor/derp/signing
-git clone https://github.com/shravansayz/private_keys.git -b rise vendor/derp/signing
-
-export BUILD_USERNAME=shravan
-export BUILD_HOSTNAME=crave
-
-# Set up build environment
-source build/envsetup.sh
-lunch derp_RMX1901-user
-make install clean
+rm -rf .repo/local_manifests && \
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 15 --git-lfs -g default,-mips,-darwin,-notdefault && \
+git clone https://github.com/shravansayz/local_manifests.git --depth 1 -b derp15 .repo/local_manifests && \
+/opt/crave/resync.sh && \
+repo forall -c 'git lfs pull' ; \
+export BUILD_USERNAME=shravan ; \
+export BUILD_HOSTNAME=android-build ; \
+export TZ=Asia/Kolkata ; \
+export DISABLE_STUB_VALIDATION=true ; \
+cd vendor/derp && curl -s https://raw.githubusercontent.com/shravansayz/ota-stuff/refs/heads/main/0001-tools-Generate-json-for-non-official-builds.patch > json.patch && git apply json.patch ; cd ../../ && \
+source build/envsetup.sh && \
+lunch derp_RMX1901-user && \
+make installclean ; \
 mka derp
