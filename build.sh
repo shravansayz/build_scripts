cc73c5baa9121889e588fbdbce0871623f7dba04
Shravankumar <shravankumarsayz@gmail.com>
2024-12-23 08:17:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,32 +1,14 @@
 #!/bin/bash
 
-# Removals
-rm -rf .repo/local_manifests
-
-# Initialize repo with specified manifest
-repo init -u https://github.com/The-Pixel-Project/manifest -b fourteen-qpr3 -g default,-mips,-darwin,-notdefault --git-lfs
-
-# Clone local_manifests repository
-git clone https://github.com/shravansayz/local_manifests --depth 1 -b tpp .repo/local_manifests
-
-# Sync the repositories
-/opt/crave/resync.sh
-
-#customs
-rm -rf frameworks/base
-git clone https://github.com/shravansayz/frameworks_base_tpp.git -b fourteen-qpr3 frameworks/base --depth=1
-
-rm -rf hardware/interfaces
-git clone https://github.com/shravansayz/hardware_interfaces.git -b fourteen-qpr3 hardware/interfaces --depth=1
-
-rm -rf vendor/aosp/signing
-git clone https://github.com/shravansayz/private_keys.git -b pixelpro vendor/aosp/signing --depth=1
-
-export BUILD_USERNAME=shravan
-export BUILD_HOSTNAME=crave
-
-#build
-source build/envsetup.sh
-lunch aosp_RMX1901-user
-make installclean
-make bacon
+rm -rf .repo/local_manifests && \
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-22.1 --git-lfs -g default,-mips,-darwin,-notdefault && \
+git clone https://github.com/shravansayz/local_manifests.git --depth 1 -b los22 .repo/local_manifests && \
+/opt/crave/resync.sh && \
+repo forall -c 'git lfs pull' ; \
+export BUILD_USERNAME=shravan ; \
+export BUILD_HOSTNAME=android-build ; \
+export TZ=Asia/Kolkata ; \
+source build/envsetup.sh && \
+lunch lineage_RMX1901-ap4a-user && \
+make installclean ; \
+mka bacon
