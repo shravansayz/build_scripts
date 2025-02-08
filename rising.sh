f7e9f2d2cd9f311d75f5adc7e3adad2aff18937a
Shravankumar <shravankumarsayz@gmail.com>
2025-02-08 18:16:25 +0530

    Update rising.sh

--- rising.sh
+++ rising.sh
@@ -1,31 +1,14 @@
 #!/bin/bash
 
-# Removals
-rm -rf .repo/local_manifests
-
-# Initialize repo with specified manifest
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-
-# Clone local_manifests repository
-git clone https://github.com/shravansayz/local_manifests --depth 1 -b rise .repo/local_manifests
-
-# Sync the repositories
-/opt/crave/resync.sh
-
-#customs
-rm -rf frameworks/base
-git clone https://github.com/shravansayz/frameworks_base_rise.git -b fourteen frameworks/base --depth=1
-
-
-#Private Keys
-rm -rf vendor/lineage-priv
-git clone https://github.com/shravansayz/private_keys.git -b rise vendor/lineage-priv
-
-export BUILD_USERNAME=shravan
-export BUILD_HOSTNAME=crave
-
-#build
-source build/envsetup.sh
-riseup RMX1901 user
-m installclean
+rm -rf .repo/local_manifests && \
+repo init -u https://github.com/RisingOS-Revived/android -b fifteen --git-lfs && \
+git clone https://github.com/shravansayz/local_manifests.git --depth 1 -b rise .repo/local_manifests && \
+/opt/crave/resync.sh && \
+repo forall -c 'git lfs pull' ; \
+export BUILD_USERNAME=shravan ; \
+export BUILD_HOSTNAME=android-build ; \
+export TZ=Asia/Kolkata ; \
+source build/envsetup.sh && \
+riseup RMX1901 user && \
+make installclean ; \
 rise b
