b13316eb18bbce939c4f5f5f988d2eb4e35b30d6
Shravankumar <shravankumarsayz@gmail.com>
2025-02-18 17:47:00 +0530

    Update pixelos.sh

--- pixelos.sh
+++ pixelos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf .repo/local_manifests && \
-repo init -u https://gitlab.com/ThankYouHonami/manifest.git -b fifteen --git-lfs && \
+repo init -u https://github.com/fiqri19102002/manifest.git -b fifteen --git-lfs && \
 git clone https://github.com/shravansayz/local_manifests.git --depth 1 -b pixel .repo/local_manifests && \
 /opt/crave/resync.sh && \
 repo forall -c 'git lfs pull' ; \
@@ -9,6 +9,6 @@ export BUILD_USERNAME=shravan ; \
 export BUILD_HOSTNAME=android-build ; \
 export TZ=Asia/Kolkata ; \
 source build/envsetup.sh && \
-breakfast RMX1901 user && \
+lunch aosp_RMX1901-ap4a-user && \
 make installclean ; \
 mka bacon && wget https://raw.githubusercontent.com/shravansayz/go-up/master/go-up && chmod +x go-up && ./go-up out/target/product/RMX1901/PixelOS*.zip
