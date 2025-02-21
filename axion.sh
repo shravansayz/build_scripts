b648f923ca8a6b1c0def5e49a8b9ae4f34a9e384
Shravankumar <shravankumarsayz@gmail.com>
2025-02-21 18:22:31 +0530

    Update and rename clover.sh to axion.sh

--- axion.sh
+++ axion.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 rm -rf .repo/local_manifests && \
-repo init -u https://github.com/Shravan55555/clover_manifest.git -b 15-qpr1 --git-lfs && \
-git clone https://github.com/shravansayz/local_manifests.git --depth 1 -b clo .repo/local_manifests && \
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.1 --git-lfs && \
+git clone https://github.com/shravansayz/local_manifests.git --depth 1 -b axion .repo/local_manifests && \
 /opt/crave/resync.sh && \
 repo forall -c 'git lfs pull' ; \
 export BUILD_USERNAME=shravan ; \
 export BUILD_HOSTNAME=android-build ; \
 export TZ=Asia/Kolkata ; \
 source build/envsetup.sh && \
-lunch clover_RMX1901-ap4a-user && \
 make installclean ; \
-mka clover
+axion RMX1901
