0dc7f2ccdea9424d1fbe7e8496240b1f59967d2d
Shravankumar <shravankumarsayz@gmail.com>
2025-08-24 17:08:48 +0530

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -4,7 +4,6 @@ rm -rf .repo/local_manifests && \
 repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs && \
 git clone https://github.com/shravansayz/local_manifests.git --depth 1 -b axion .repo/local_manifests && \
 /opt/crave/resync.sh && \
-repo forall -c 'git lfs pull' ; \
 export BUILD_USERNAME=shravan ; \
 export BUILD_HOSTNAME=android-build ; \
 export TZ=Asia/Kolkata ; \
