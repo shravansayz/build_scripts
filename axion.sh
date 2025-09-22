c206562633403f3b991ab8a1806259b4e2c0f38a
Shravankumar <shravankumarsayz@gmail.com>
2025-09-22 09:34:37 +0530

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -11,5 +11,5 @@ wget https://github.com/shravansayz/local_manifests/raw/keys/keys.zip && unzip -
 echo ">>> Applying frameworks/base patch..." && cd frameworks/base && (git log --oneline | grep -q "dt2w\|DT2W\|double.*tap" || (wget -O temp.patch "https://github.com/shravansayz/android_frameworks_base/commit/7809e13937efaf85b319bc28d3b88326342ec1df.patch" && (git apply temp.patch && git add . && git commit -m "Apply DT2W patch" || echo "Patch conflicts detected, continuing build...") && rm -f temp.patch)) && cd ../.. && echo ">>> Frameworks/base patch process completed!" && \
 source build/envsetup.sh && \
 axion RMX1901 user gms pico && \
-make installclean ; \
+make installclean && \
 ax -br
