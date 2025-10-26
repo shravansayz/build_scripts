13af82f54d4bfec85823c8c0f93b23a9c1c3be47
Shravankumar <shravankumarsayz@gmail.com>
2025-10-26 10:24:43 +0530

    Update luna.sh

--- luna.sh
+++ luna.sh
@@ -9,7 +9,7 @@ export BUILD_HOSTNAME=crave && \
 export TZ=Asia/Kolkata && \
 wget https://github.com/shravansayz/local_manifests/raw/keys/keys.zip && unzip -o keys.zip -d vendor/lineage-priv/ && rm keys.zip && \
 echo ">>> Applying frameworks/base patch..." && cd frameworks/base && (git log --oneline | grep -q "dt2w\|DT2W\|double.*tap" || (wget -O temp.patch "https://github.com/shravansayz/android_frameworks_base/commit/7809e13937efaf85b319bc28d3b88326342ec1df.patch" && (git apply temp.patch && git add . && git commit -m "Apply DT2W patch" || echo "Patch conflicts detected, continuing build...") && rm -f temp.patch)) && cd ../.. && echo ">>> Frameworks/base patch process completed!" && \
-source build/envsetup.sh && \
+source b*/env* && \
 lunch lineage_RMX1901-bp2a-user && \
 make installclean ; \
 m lunaris
