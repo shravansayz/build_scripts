ef94bebd8cf2cdff85f6dcb62ccc9ca4a12c3081
Shravankumar <shravankumarsayz@gmail.com>
2025-10-26 12:30:24 +0530

    Update download path for keys in luna.sh

--- luna.sh
+++ luna.sh
@@ -7,7 +7,7 @@ git clone https://github.com/shravansayz/local_manifests.git --depth 1 -b luna16
 export BUILD_USERNAME=shravan && \
 export BUILD_HOSTNAME=crave && \
 export TZ=Asia/Kolkata && \
-wget https://github.com/shravansayz/local_manifests/raw/keys/keys.zip && unzip -o keys.zip -d vendor/lineage-priv/ && rm keys.zip && \
+wget https://github.com/shravansayz/local_manifests/raw/keys/keys.zip && unzip -o keys.zip -d vendor/lunaris-priv/ && rm keys.zip && \
 echo ">>> Applying frameworks/base patch..." && cd frameworks/base && (git log --oneline | grep -q "dt2w\|DT2W\|double.*tap" || (wget -O temp.patch "https://github.com/shravansayz/android_frameworks_base/commit/7809e13937efaf85b319bc28d3b88326342ec1df.patch" && (git apply temp.patch && git add . && git commit -m "Apply DT2W patch" || echo "Patch conflicts detected, continuing build...") && rm -f temp.patch)) && cd ../.. && echo ">>> Frameworks/base patch process completed!" && \
 . b*/env* && \
 lunch lineage_RMX1901-bp2a-user && \
